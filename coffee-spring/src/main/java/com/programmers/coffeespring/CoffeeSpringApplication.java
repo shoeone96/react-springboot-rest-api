9638402461509c5320caae80b4201ceea1227781
shoeone96 <dlwnddnjs96@gmail.com>
2023-11-09 23:05:49 +0900

    chore: spring 기본 세팅

---
+++ coffee-spring/src/main/java/com/programmers/coffeespring/CoffeeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.programmers.coffeespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeSpringApplication.class, args);
+	}
+
+}
