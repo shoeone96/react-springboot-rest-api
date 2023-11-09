38a11f2c49ea2938cbe19f0872e79af8fde682e8
shoeone96 <dlwnddnjs96@gmail.com>
2023-11-09 22:51:08 +0900

    feat: product 도메인 모델 클래스 작성

---
+++ coffee-spring/src/main/java/com/programmers/coffeespring/model/Product.java
@@ -0,0 +1,68 @@
+package com.programmers.coffeespring.model;
+
+import com.programmers.coffeespring.dto.ProductCreateRequestDto;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Product {
+    private Long productId;
+    private String productName;
+    private Category category;
+    private long price;
+    private String productImg;
+    private String description;
+    private final LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    private Product(String productName, Category category, long price, String productImg, String description) {
+        this.productName = productName;
+        this.category = category;
+        this.price = price;
+        this.productImg = productImg;
+        this.description = description;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = createdAt;
+    }
+
+
+    public static Product fromCreateDto(ProductCreateRequestDto requestDto){
+        return new Product(
+                requestDto.productName(),
+                requestDto.category(),
+                requestDto.price(),
+                requestDto.productImg(),
+                requestDto.description()
+                );
+    }
+
+    public static Product fromDbMapper(Long productId, String productName, Category category, long price, String productImg, String description, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        return new Product(
+                productId,
+                productName,
+                category,
+                price,
+                productImg,
+                description,
+                createdAt,
+                updatedAt
+        );
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product product)) return false;
+        return price == product.price && Objects.equals(productId, product.productId) && Objects.equals(productName, product.productName) && category == product.category && Objects.equals(productImg, product.productImg) && Objects.equals(description, product.description) && Objects.equals(createdAt, product.createdAt) && Objects.equals(updatedAt, product.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productId, productName, category, price, productImg, description, createdAt, updatedAt);
+    }
+}
