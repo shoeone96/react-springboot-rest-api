c6d0ed68914b2f3a3fc3fa3a37583c23e3b81b64
shoeone96 <dlwnddnjs96@gmail.com>
2023-11-09 23:17:24 +0900

    chore: 기본 테스트

---
+++ coffee-spring/src/test/java/com/programmers/coffeespring/CoffeeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmers.coffeespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
