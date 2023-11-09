c9989b7c02f995e71ca807777816dec74f46b8d8
shoeone96 <dlwnddnjs96@gmail.com>
2023-11-09 21:57:58 +0900

    feat: 모든 product를 전달하는 api 작성

---
+++ coffee-spring/src/main/java/com/programmers/coffeespring/controller/ProductController.java
@@ -0,0 +1,25 @@
+package com.programmers.coffeespring.controller;
+
+import com.programmers.coffeespring.dto.ProductCreateRequestDto;
+import com.programmers.coffeespring.dto.ProductResponseDto;
+import com.programmers.coffeespring.dto.Response;
+import com.programmers.coffeespring.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    @GetMapping("/api/v1/products")
+    public Response<List<ProductResponseDto>> getAllProductList(){
+        return Response.success(productService.getAllProducts());
+    }
+    
+}
