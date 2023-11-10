a8ddacae836e807233845bb78ba5f8f1b0e9edc8
shoeone96 <dlwnddnjs96@gmail.com>
2023-11-10 21:30:04 +0900

    feat: errorcode 추가

--- coffee-spring/src/main/java/com/programmers/coffeespring/exception/ErrorCode.java
+++ coffee-spring/src/main/java/com/programmers/coffeespring/exception/ErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 에러가 발생했습니다."),
     INVALID_INPUT(HttpStatus.BAD_REQUEST, "잘못 입력하였습니다. 다시 입력해주세요"),
-    IMG_SAVE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 저장 중 문제가 발생했습니다.");
+    IMG_SAVE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 저장 중 문제가 발생했습니다."),
+    COMPRESS_IMG_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 저장을 위한 압축 과정에서 에러가 발생했습니다.");
     private final HttpStatus status;
     private final String errorMessage;
 }
