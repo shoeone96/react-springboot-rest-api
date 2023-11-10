873e5898e45c157b95eca3a2fc5bb94751cc88b0
shoeone96 <dlwnddnjs96@gmail.com>
2023-11-10 13:46:38 +0900

    feat: docker로 mysql 설정
    프로파일로 다른 db schema 사용하도록 설정

---
+++ coffee-spring/mysql/init.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE IF NOT EXISTS coffee;
+
+USE coffee;
+
+CREATE TABLE `products` (
+                            `product_id` bigint NOT NULL AUTO_INCREMENT,
+                            `product_name` varchar(20) NOT NULL,
+                            `category` varchar(50) NOT NULL,
+                            `price` bigint NOT NULL,
+                            `description` varchar(50) DEFAULT NULL,
+                            `product_img` varchar(255) DEFAULT NULL,
+                            `created_at` datetime(6) NOT NULL,
+                            `updated_at` datetime(6) DEFAULT NULL,
+                            PRIMARY KEY (`product_id`)
+);
+
+
+
+CREATE DATABASE IF NOT EXISTS coffee_test;
+
+USE coffee_test;
+
+CREATE TABLE `products` (
+                            `product_id` bigint NOT NULL AUTO_INCREMENT,
+                            `product_name` varchar(20) NOT NULL,
+                            `category` varchar(50) NOT NULL,
+                            `price` bigint NOT NULL,
+                            `description` varchar(50) DEFAULT NULL,
+                            `product_img` varchar(255) DEFAULT NULL,
+                            `created_at` datetime(6) NOT NULL,
+                            `updated_at` datetime(6) DEFAULT NULL,
+                            PRIMARY KEY (`product_id`)
+);
+
