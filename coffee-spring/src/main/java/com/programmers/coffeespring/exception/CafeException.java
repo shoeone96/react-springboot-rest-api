91e0c6e3a7710eaeb982a9936c2dd64d04a43804
shoeone96 <dlwnddnjs96@gmail.com>
2023-11-10 13:47:26 +0900

    feat: 이미지 저장 중 발생할 수 있는 예외 처리

--- coffee-spring/src/main/java/com/programmers/coffeespring/exception/CafeException.java
+++ coffee-spring/src/main/java/com/programmers/coffeespring/exception/CafeException.java
@@ -4,8 +4,12 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class CafeException extends RuntimeException{
     private final ErrorCode errorCode;
     private final String message;
+
+    public CafeException(ErrorCode errorCode){
+        this.errorCode = errorCode;
+        this.message = errorCode.getErrorMessage();
+    }
 }
