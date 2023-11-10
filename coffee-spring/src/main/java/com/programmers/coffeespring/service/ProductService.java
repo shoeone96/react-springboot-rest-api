9e0faed5c6de2830c5f808ca7630e6eadfaf9988
shoeone96 <dlwnddnjs96@gmail.com>
2023-11-10 21:27:27 +0900

    refactor: 이미지 저장 및 조회 로직 변경
    base64 기반의 로직

--- coffee-spring/src/main/java/com/programmers/coffeespring/service/ProductService.java
+++ coffee-spring/src/main/java/com/programmers/coffeespring/service/ProductService.java
@@ -2,13 +2,23 @@ package com.programmers.coffeespring.service;
 
 import com.programmers.coffeespring.dto.ProductCreateRequestDto;
 import com.programmers.coffeespring.dto.ProductResponseDto;
+import com.programmers.coffeespring.exception.CafeException;
+import com.programmers.coffeespring.exception.ErrorCode;
 import com.programmers.coffeespring.model.Product;
 import com.programmers.coffeespring.repository.ProductRepository;
 import com.programmers.coffeespring.util.ImgUtil;
+import javafx.scene.image.Image;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
 
 @Service
 @RequiredArgsConstructor
@@ -22,8 +32,14 @@ public class ProductService {
     }
 
     public void saveNewProduct(ProductCreateRequestDto requestDto) {
-        String img = ImgUtil.saveImage(requestDto.productImg());
-        Product product = Product.fromCreateDto(requestDto, img);
-        productRepository.save(product);
+        try {
+            byte[] imgBytes = requestDto.productImg().getBytes();
+            Product product = Product.fromCreateDto(requestDto, imgBytes);
+            productRepository.save(product);
+        } catch (IOException e) {
+            throw new CafeException(ErrorCode.COMPRESS_IMG_ERROR);
+        }
     }
+
+
 }
--- coffee-spring/src/main/java/com/programmers/coffeespring/util/ImgUtil.java
+++ coffee-spring/src/main/java/com/programmers/coffeespring/util/ImgUtil.java
@@ -4,38 +4,49 @@ import com.programmers.coffeespring.exception.CafeException;
 import com.programmers.coffeespring.exception.ErrorCode;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
 
 public class ImgUtil {
-    private static final String IMG_PATH ="src/main/resources/static/images/";
-
-    public static String saveImage(MultipartFile file) {
-        File directory = new File(IMG_PATH);
-        noDirectoryCase(directory);
-
-        if(file.isEmpty()){
-            return "images/default_coffee.png";
-        }
+    public static byte[] decompressBytes(byte[] data) {
+        Inflater inflater = new Inflater();
+        inflater.setInput(data);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+        byte[] buffer = new byte[1024];
         try {
-            String fileName = saveFile(file);
-            return "images/" + fileName;
-        } catch (IOException e){
-            throw new CafeException(ErrorCode.IMG_SAVE_ERROR);
+            while (!inflater.finished()) {
+                int count = inflater.inflate(buffer);
+                outputStream.write(buffer, 0, count);
+            }
+            outputStream.close();
+        } catch (IOException | DataFormatException ignored) {
         }
+
+        return outputStream.toByteArray();
     }
 
-    private static void noDirectoryCase(File directory) {
-        if (!directory.exists()) {
-            directory.mkdirs();
-        }
-    }
 
-    private static String saveFile(MultipartFile file) throws IOException {
-        String fileName = file.getOriginalFilename();
-        String filePath = IMG_PATH + fileName;
-        File dest = new File(filePath);
-        file.transferTo(dest);
-        return fileName;
+    public static byte[] compressBytes(byte[] data) {
+        Deflater deflater = new Deflater();
+        deflater.setInput(data);
+        deflater.finish();
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+        byte[] buffer = new byte[1024];
+        while (!deflater.finished()) {
+            int count = deflater.deflate(buffer);
+            outputStream.write(buffer, 0, count);
+
+            try {
+                outputStream.close();
+            } catch (IOException e) {
+                throw new CafeException(ErrorCode.IMG_SAVE_ERROR);
+            }
+        }
+        return outputStream.toByteArray();
     }
 }
