049ea3dd4dd95ca7c71505bd54f48616f64f6707
shoeone96 <dlwnddnjs96@gmail.com>
2023-11-10 13:48:37 +0900

    feat: 이미지 저장 중 발생할 수 있는 예외 추가

--- coffee-spring/src/main/java/com/programmers/coffeespring/exception/ErrorCode.java
+++ coffee-spring/src/main/java/com/programmers/coffeespring/exception/ErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 에러가 발생했습니다."),
-    INVALID_INPUT(HttpStatus.BAD_REQUEST, "잘못 입력하였습니다. 다시 입력해주세요");
+    INVALID_INPUT(HttpStatus.BAD_REQUEST, "잘못 입력하였습니다. 다시 입력해주세요"),
+    IMG_SAVE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 저장 중 문제가 발생했습니다.");
     private final HttpStatus status;
     private final String errorMessage;
 }
