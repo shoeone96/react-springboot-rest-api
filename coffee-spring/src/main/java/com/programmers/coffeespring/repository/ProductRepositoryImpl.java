bff6fe5d3b1a182a716538b85ae9db402c77d715
shoeone96 <dlwnddnjs96@gmail.com>
2023-11-12 19:45:43 +0900

    refactor: jdbcutil의 메서드를 이용하는 방식으로 변경

--- coffee-spring/src/main/java/com/programmers/coffeespring/repository/ProductRepositoryImpl.java
+++ coffee-spring/src/main/java/com/programmers/coffeespring/repository/ProductRepositoryImpl.java
@@ -2,19 +2,17 @@ package com.programmers.coffeespring.repository;
 
 import com.programmers.coffeespring.model.Category;
 import com.programmers.coffeespring.model.Product;
+import com.programmers.coffeespring.util.JdbcUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
@@ -25,7 +23,7 @@ public class ProductRepositoryImpl implements ProductRepository {
     @Override
     public List<Product> findAll() {
         return jdbcTemplate.query("select * from products",
-                productRowMapper);
+                JdbcUtils.productRowMapper);
     }
 
     @Override
@@ -33,9 +31,9 @@ public class ProductRepositoryImpl implements ProductRepository {
         int update = jdbcTemplate.update(
                 "insert into products(product_name, category, price, description, product_img, created_at, updated_at) " +
                         "values(:productName, :category, :price, :description, :productImg, :createdAt, :updatedAt)",
-                insertMap(product)
+                JdbcUtils.productInsertMap(product)
         );
-        if(update != 1){
+        if (update != 1) {
             throw new RuntimeException("Nothing was inserted");
         }
     }
@@ -45,29 +43,24 @@ public class ProductRepositoryImpl implements ProductRepository {
         jdbcTemplate.update("delete from products", Collections.emptyMap());
     }
 
-    private Map<String, Object> insertMap(Product product) {
-        Map<String, Object> map = new ConcurrentHashMap<>();
-        map.put("productName", product.getProductName());
-        map.put("category", product.getCategory().name());
-        map.put("price", product.getPrice());
-        map.put("description", product.getDescription());
-        map.put("productImg", product.getProductImg());
-        map.put("createdAt", product.getCreatedAt());
-        map.put("updatedAt", product.getUpdatedAt());
-        return map;
+    @Override
+    public Optional<Product> findById(long productId) {
+        return Optional.ofNullable(jdbcTemplate.queryForObject(
+                "select * from products where product_id = :productId",
+                Collections.singletonMap("productId", productId)
+                , JdbcUtils.productRowMapper
+        ));
     }
 
-    private final RowMapper<Product> productRowMapper = (resultSet, i) -> Product.fromDbMapper(
-            resultSet.getLong("product_id"),
-            resultSet.getString("product_name"),
-            Category.valueOf(resultSet.getString("category")),
-            resultSet.getLong("price"),
-            resultSet.getBytes("product_img"),
-            resultSet.getString("description"),
-            resultSet.getObject("created_at", LocalDateTime.class),
-            resultSet.getObject("updated_at", LocalDateTime.class)
-    );
+    @Override
+    public List<Product> findAllByCategory(Category category) {
+        return jdbcTemplate.query(
+                "select * from products where category = :category",
+                Collections.singletonMap("category", category.name())
+                , JdbcUtils.productRowMapper
+        );
 
+    }
 
 
 }
