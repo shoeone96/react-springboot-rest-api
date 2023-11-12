c7f1c7d77f89f6e579291e0a14462ba2a8b7834a
shoeone96 <dlwnddnjs96@gmail.com>
2023-11-12 19:51:23 +0900

    feat: product 생성 및 조회 로직 수정, 매핑 정보

--- coffee-spring/src/main/java/com/programmers/coffeespring/controller/ProductController.java
+++ coffee-spring/src/main/java/com/programmers/coffeespring/controller/ProductController.java
@@ -3,26 +3,30 @@ package com.programmers.coffeespring.controller;
 import com.programmers.coffeespring.dto.ProductCreateRequestDto;
 import com.programmers.coffeespring.dto.ProductResponseDto;
 import com.programmers.coffeespring.dto.Response;
+import com.programmers.coffeespring.model.Category;
 import com.programmers.coffeespring.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
 
-    @GetMapping("/api/v1/products")
-    public Response<List<ProductResponseDto>> getAllProductList(){
-        return Response.success(productService.getAllProducts());
+    @GetMapping
+    public Response<List<ProductResponseDto>> getProductsByCategory(
+            @RequestParam Category category)
+    {
+        return Response.success(productService.getProductsByCategory(category));
     }
 
-    @PostMapping("/api/v1/products")
-    public Response<Void> saveProducts(@ModelAttribute ProductCreateRequestDto requestDto){
+    @PostMapping
+    public Response<Void> saveProducts(
+            @ModelAttribute ProductCreateRequestDto requestDto)
+    {
         productService.saveNewProduct(requestDto);
         return Response.success();
     }
