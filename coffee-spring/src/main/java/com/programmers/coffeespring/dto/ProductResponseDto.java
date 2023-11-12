3198f4aeffa47f38bd8c106a104a9aa8e45c95f9
shoeone96 <dlwnddnjs96@gmail.com>
2023-11-12 19:51:48 +0900

    feat: product 검증 로직 추가

--- coffee-spring/src/main/java/com/programmers/coffeespring/dto/ProductResponseDto.java
+++ coffee-spring/src/main/java/com/programmers/coffeespring/dto/ProductResponseDto.java
@@ -2,16 +2,17 @@ package com.programmers.coffeespring.dto;
 
 import com.programmers.coffeespring.model.Category;
 import com.programmers.coffeespring.model.Product;
+import org.springframework.lang.NonNull;
 
 import java.util.Base64;
 
 public record ProductResponseDto(
-        Long productId,
-        String productName,
-        Category category,
-        long price,
-        String productImg,
-        String description) {
+        @NonNull Long productId,
+        @NonNull String productName,
+        @NonNull Category category,
+        @NonNull long price,
+        @NonNull String productImg,
+        @NonNull String description) {
     public static ProductResponseDto of(Product product) {
         String productImg = Base64.getEncoder().encodeToString(product.getProductImg());
         return new ProductResponseDto(
